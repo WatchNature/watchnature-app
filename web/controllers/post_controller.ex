a8f709eca3a23cc9e4008cf05423d3002d8fee7c
Sean Washington <hello@seanwash.com>
2017-03-04 15:54:34 -0500

    Enforce auth for post CRUD

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -3,6 +3,7 @@ defmodule Watchnature.PostController do
 
   alias Watchnature.Post
 
+  plug Guardian.Plug.EnsureAuthenticated, [handler: Watchnature.AuthController] when action in [:create, :update, :delete]
   plug :scrub_params, "post" when action in [:create, :update]
 
   def index(conn, _params) do
