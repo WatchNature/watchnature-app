dfa09a726713f81bfb26bac4fcc2d7796b317f2a
Sean Washington <hello@seanwash.com>
2017-03-07 21:54:31 -0500

    Scrub location params before casting to keep Geo from throwing with empty coordinate array

--- web/models/post.ex
+++ web/models/post.ex
@@ -1,8 +1,6 @@
 defmodule Watchnature.Post do
   use Watchnature.Web, :model
 
-  require IEx
-
   schema "posts" do
     field :description, :string
     field :location, Geo.Point
@@ -14,6 +12,8 @@ defmodule Watchnature.Post do
   end
 
   def changeset(struct, params \\ %{}) do
+    params = remove_location_if_empty(params)
+
     struct
     |> cast(params, [:description, :location, :location_name, :user_id])
     |> validate_required([:description])
@@ -23,5 +23,15 @@ defmodule Watchnature.Post do
   def sorted(query \\ __MODULE__) do
     from p in query,
     order_by: [desc: p.inserted_at]
+  end
+
+  # If location map is empty (nothing sent by client) then remove
+  # that map from the prams altogehter as Geo will throw when it
+  # tries to parse an empty coordinates array
+  defp remove_location_if_empty(params) do
+    case params do
+      %{"location" => %{"coordinates" => [], "type" => "Point"}} -> Map.delete(params, "location")
+      _ -> params
+    end
   end
 end
