4a0fd21c550d2155b0383049d6948a71c815495e
Sean Washington <hello@seanwash.com>
2017-03-12 17:56:44 -0400

    Add seeds for two user accounts

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Watchnature.{Repo, User}
+
+Repo.insert!(User.registration_changeset(%User{}, %{email: "sean@watchnature.co", password: "secret"}))
+Repo.insert!(User.registration_changeset(%User{}, %{email: "brady@watchnature.co", password: "secret"}))
