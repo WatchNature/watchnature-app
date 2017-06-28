3d9df7efa60e5b010290448af23d08e36db15d83
Sean Washington <hello@seanwash.com>
2017-06-27 22:20:42 -0400

    Remove test case for nonexistant invalid post case

--- test/models/post_test.exs
+++ test/models/post_test.exs
@@ -4,15 +4,9 @@ defmodule Watchnature.PostTest do
   alias Watchnature.Post
 
   @valid_attrs %{description: "some content"}
-  @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = Post.changeset(%Post{}, @valid_attrs)
     assert changeset.valid?
-  end
-
-  test "changeset with invalid attributes" do
-    changeset = Post.changeset(%Post{}, @invalid_attrs)
-    refute changeset.valid?
   end
 end
