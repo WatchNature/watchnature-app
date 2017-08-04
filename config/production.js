dc9e05295b13bee21f84b33d0c771de15810628d
Sean Washington <hello@seanwash.com>
2017-08-03 22:42:11 -0700

    Again

---
+++ config/production.js
@@ -0,0 +1,12 @@
+module.exports = {
+  URL: JSON.stringify('https://wn=app-staging.herokuapp.com'),
+  ENV: JSON.stringify('production'),
+  API_BASE_URL: JSON.stringify('https://wn-app-staging.herokuapp.com/api'),
+  APP_BASE_URL: JSON.stringify('https://wn-app-staging.herokuapp.com'),
+  GOOGLE_MAPS_API_KEY: JSON.stringify('AIzaSyCVahUUvHx_ir4b0NE9_TiENsnJ4P8Ncz8'),
+  IMGIX_SOURCE: JSON.stringify(''),
+  IMGIX_SOURCE_HOSTNAME: JSON.stringify(''),
+  DOCUMENT_TITLE_BASE: JSON.stringify('[STAGE] Watch Nature'),
+  DOCUMENT_TITLE_SEPARATOR: JSON.stringify('»'),
+  GA_ID: JSON.stringify('')
+}
