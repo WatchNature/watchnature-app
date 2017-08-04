c82ff6ff4442716d692e1bea5096d749614bd647
Sean Washington <hello@seanwash.com>
2017-08-04 10:40:53 -0700

    More debugging for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :watchnature, Watchnature.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 config :watchnature, Watchnature.Repo,
   adapter: Ecto.Adapters.Postgres,
