acc32862cb34bb07b45a685e97f4419fdb00ce23
Sean Washington <hello@seanwash.com>
2017-08-10 15:07:42 -0700

    Add Post.paginate_posts function & add to controller. Closes #41
    - Function retuns a Scrivener.Page
    struct
    - Add authorize function for Post.paginate_posts
    - Update PostController to use paginate_posts

--- lib/watchnature/stream/stream_policy.ex
+++ lib/watchnature/stream/stream_policy.ex
@@ -16,6 +16,7 @@ defmodule Watchnature.Stream.Policy do
     and user_id == post_user_id, do: :ok
 
   def authorize(:list_posts, _user, _), do: :ok
+  def authorize(:paginate_posts, _user, _), do: :ok
   def authorize(:get_post, _user, post), do: :ok
 
   def authorize(_, _, _), do: {:error, :unauthorized}
