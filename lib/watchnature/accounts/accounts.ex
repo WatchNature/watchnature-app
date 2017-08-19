1202d22986d2cc480ab1ce7ee90f6125abb0faa2
Sean Washington <hello@seanwash.com>
2017-08-19 15:26:09 -0700

    Move user actions to Accounts context

--- lib/watchnature/accounts/accounts.ex
+++ lib/watchnature/accounts/accounts.ex
@@ -6,7 +6,107 @@ defmodule Watchnature.Accounts do
   import Ecto.Query, warn: false
   alias Watchnature.Repo
 
-  alias Watchnature.Accounts.Group
+  alias Watchnature.Accounts.{User, Group}
+
+  #
+  # Users
+  #
+
+  @doc """
+  Returns the list of users.
+
+  ## Examples
+
+      iex> list_user()
+      [%User{}, ...]
+
+  """
+  def list_users, do: Repo.all(User)
+
+  @doc """
+  Gets a single user.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user!(123)
+      %Group{}
+
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user!(id), do: Repo.get!(User, id)
+
+  @doc """
+  Registers a user.
+
+  ## Examples
+
+  iex> register_user(%{field: value})
+  {:ok, %User{}}
+
+  iex> register_user(%{field: bad_value})
+  {:error, %Ecto.Changeset{}}
+
+  """
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a user.
+
+  ## Examples
+
+      iex> update_user(user, %{field: new_value})
+      {:ok, %User{}}
+
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a User.
+
+  ## Examples
+
+      iex> delete_user(user)
+      {:ok, %User{}}
+
+      iex> delete_user(user)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user changes.
+
+  ## Examples
+
+      iex> change_user(user)
+      %Ecto.Changeset{source: %User{}}
+
+  """
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  #
+  # Groups
+  #
 
   @doc """
   Returns the list of groups.
