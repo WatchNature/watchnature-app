b78802f4b1ac10cfe8efd4371583dba6abbfdc76
Sean Washington <hello@seanwash.com>
2017-08-30 08:55:46 -0400

    Add Reactions context and ObservationLike schema

---
+++ lib/watchnature/reactions/reactions.ex
@@ -0,0 +1,50 @@
+defmodule Watchnature.Reactions do
+  @moduledoc """
+  The Reactions context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Watchnature.Repo
+
+  alias Watchnature.Stream
+  alias Watchnature.Accounts
+  alias Watchnature.Reactions.ObservationLike
+
+  @doc """
+  Returns a like between the given user and entity.
+  """
+  def get_like!(%Accounts.User{} = user, %Stream.Observation{} = observation) do
+    query = from ol in ObservationLike,
+              where: ol.user_id == ^user.id and ol.observation_id == ^observation.id
+
+    Repo.one!(query)
+  end
+
+  @doc """
+  Creates a like between the given user and entity.
+
+  ## Examples
+
+      iex>add_like(%User{}, %Observation{})
+      %ObservationLike{}
+
+  """
+  def add_like(%Accounts.User{} = user, %Stream.Observation{} = observation),
+    do: create_observation_like(%{user: user, observation: observation})
+
+  @doc """
+  Deletes the given ObservationLike
+  """
+  def delete_like(%ObservationLike{} = observation_like),
+    do: delete_observation_like(observation_like)
+
+  defp create_observation_like(attrs \\ %{}) do
+    %ObservationLike{}
+    |> ObservationLike.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  defp delete_observation_like(%ObservationLike{} = observation_like) do
+    Repo.delete(observation_like)
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -3,6 +3,9 @@ defmodule Watchnature.Factory do
 
   use Watchnature.Accounts.UserFactory
   use Watchnature.Accounts.GroupFactory
+
   use Watchnature.Stream.PostFactory
   use Watchnature.Stream.ObservationFactory
+
+  use Watchnature.Reactions.LikeFactory
 end
