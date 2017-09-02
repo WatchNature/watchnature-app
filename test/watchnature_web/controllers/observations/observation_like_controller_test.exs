429306dcca913cc0ac0a5d646d48535348086740
Sean Washington <hello@seanwash.com>
2017-09-02 10:17:54 -0400

    Test for owned and unowned like delete

--- test/watchnature_web/controllers/observations/observation_like_controller_test.exs
+++ test/watchnature_web/controllers/observations/observation_like_controller_test.exs
@@ -36,7 +36,7 @@ defmodule WatchnatureWeb.ObservationLikeControllerTest do
   end
 
   describe "delete/2" do
-    test "a like is deleted between the user and observation", %{conn: conn, jwt: jwt, user: user} do
+    test "a like is deleted between the user and observation when the user owns the like", %{conn: conn, jwt: jwt, user: user} do
       post = insert(:stream_post)
       observation = insert(:stream_observation)
       observation_like = insert(:reactions_observation_like, %{user: user, observation: observation})
@@ -46,6 +46,19 @@ defmodule WatchnatureWeb.ObservationLikeControllerTest do
       |> delete(post_observation_observation_like_path(conn, :create, post, observation))
 
       assert response(conn, 204)
+    end
+
+    test "a like is deleted between the user and observation when the user does not own the like", %{conn: conn, jwt: jwt, user: user} do
+      owner_user = insert(:accounts_user)
+      post = insert(:stream_post)
+      observation = insert(:stream_observation)
+      observation_like = insert(:reactions_observation_like, %{user: owner_user, observation: observation})
+
+      conn = build_conn()
+      |> put_req_header("authorization", "Bearer #{jwt}")
+      |> delete(post_observation_observation_like_path(conn, :create, post, observation))
+
+      assert response(conn, 403)
     end
   end
 end
